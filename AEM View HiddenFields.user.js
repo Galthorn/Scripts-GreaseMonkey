727aecb386eb9717ab42ee1b46a911240421764b
Galthorn <galthorn@hotmail.com>
2016-06-17 13:50:09 +0200

    Add all fields

--- AEM View HiddenFields.user.js
+++ AEM View HiddenFields.user.js
@@ -12,13 +12,12 @@
 var load,execute,loadAndExecute;load=function(a,b,c){var d;d=document.createElement("script"),d.setAttribute("src",a),b!=null&&d.addEventListener("load",b),c!=null&&d.addEventListener("error",c),document.body.appendChild(d);return d},execute=function(a){var b,c;typeof a=="function"?b="("+a+")();":b=a,c=document.createElement("script"),c.textContent=b,document.body.appendChild(c);return c},loadAndExecute=function(a,b){return load(a,function(){return execute(b)})};
 
 loadAndExecute("//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js", function() {
-    //$('[type=hidden]').attr('name');
-    //console.log($('[type=hidden]'));
     $( document ).ready(function() {
-        $('input[id^=hidden]').each(function() {
+        //$('#cdx').after('<div><div style="position:relative;">testJL</div></div>');
+        $('.cq-wcm-preview input, .cq-wcm-preview select, .cq-wcm-preview textarea').each(function() { // [id^=hidden]
             var hiddenName = jQuery(this).attr("name");
-            jQuery(this).after(" -> "+hiddenName);
-            //test update
+            jQuery(this).parent().append("<span class='heliosKeyHack' style='color: red;'> -> "+hiddenName+"</span>");
         });
     });
 });
+
